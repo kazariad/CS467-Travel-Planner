1cf5fb227b2d3ed471949f46ef2df7f05ea8b8a3
David K <122331465+kazariad@users.noreply.github.com>
2025-01-23 01:53:48 -0800

    Init

---
+++ src/test/java/edu/oregonstate/cs467/travelplanner/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.oregonstate.cs467.travelplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
