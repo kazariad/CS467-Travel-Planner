20562b26623ec0a1fe74787ce972c3889f72a2ee
theadinh59 <theadinh59@gmail.com>
2025-01-26 17:47:08 -0600

    Add user table definition and user_experience table definition.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -21,4 +21,25 @@ CREATE TABLE `experience`  (
   FULLTEXT INDEX `title_description_address_ft_idx`(`title`, `description`, `address`)
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+DROP TABLE IF EXISTS `user_experience`;
+CREATE TABLE `user_experience`  (
+  `user_id` bigint NOT NULL,
+  `experience_id` bigint NOT NULL,
+  PRIMARY KEY (`user_id`, `experience_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`) ON DELETE CASCADE,
+  FOREIGN KEY (`experience_id`) REFERENCES `experience`(`experience_id`) ON DELETE CASCADE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
 SET FOREIGN_KEY_CHECKS = 1;
