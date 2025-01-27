de758da710d6d78ce1a81d8c5f7fda798026f614
David K <122331465+kazariad@users.noreply.github.com>
2025-01-27 03:22:35 -0800

    Add Experience and GeoPoint models

---
+++ src/main/java/edu/oregonstate/cs467/travelplanner/experience/model/Experience.java
@@ -0,0 +1,136 @@
+package edu.oregonstate.cs467.travelplanner.experience.model;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+public class Experience {
+    private Long experienceId;
+
+    private String title;
+
+    private String description;
+
+    private LocalDate eventDate;
+
+    private GeoPoint location;
+
+    private String address;
+
+    private String imageUrl;
+
+    private int ratingCnt;
+
+    private int ratingSum;
+
+    private long userId;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    private Instant deletedAt;
+
+    public Long getExperienceId() {
+        return experienceId;
+    }
+
+    public void setExperienceId(Long experienceId) {
+        this.experienceId = experienceId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public GeoPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoPoint location) {
+        this.location = location;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getRatingCnt() {
+        return ratingCnt;
+    }
+
+    public void setRatingCnt(int ratingCnt) {
+        this.ratingCnt = ratingCnt;
+    }
+
+    public int getRatingSum() {
+        return ratingSum;
+    }
+
+    public void setRatingSum(int ratingSum) {
+        this.ratingSum = ratingSum;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Instant deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}
