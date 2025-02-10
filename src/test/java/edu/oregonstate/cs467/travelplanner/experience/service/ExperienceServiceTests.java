f7d1061a75fbe2aa56a9e7d2c692cae97f90cb42
David K <122331465+kazariad@users.noreply.github.com>
2025-02-10 12:39:07 -0800

    Refactor resource directories to proper case to match class names

--- src/test/java/edu/oregonstate/cs467/travelplanner/experience/service/ExperienceServiceTests.java
+++ src/test/java/edu/oregonstate/cs467/travelplanner/experience/service/ExperienceServiceTests.java
@@ -2,37 +2,49 @@ package edu.oregonstate.cs467.travelplanner.experience.service;
 
 import edu.oregonstate.cs467.travelplanner.AbstractBaseTest;
 import edu.oregonstate.cs467.travelplanner.experience.model.Experience;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlMergeMode;
 import org.springframework.test.context.jdbc.SqlMergeMode.MergeMode;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SqlMergeMode(MergeMode.MERGE)
 @Sql(scripts = "file:sql/schema.sql", executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
-@Sql(scripts = "/sql/user/1.sql", executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
-@Sql(scripts = "/sql/user/2.sql", executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = "/sql/User/1.sql", executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql(scripts = "/sql/User/2.sql", executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
 @Sql(statements = "ALTER TABLE experience AUTO_INCREMENT = 1")
 class ExperienceServiceTests extends AbstractBaseTest {
     @Autowired
     private ExperienceService service;
 
+    @BeforeEach
+    void beforeEach() {
+        Mockito.doReturn(Instant.now().truncatedTo(ChronoUnit.MICROS)).when(clock).instant();
+    }
+
     @Test
-    @Sql(scripts = "/sql/experience/1.sql")
-    @Sql(scripts = "/sql/experience/2.sql")
-    void getExperience() throws Exception {
-        var actual = service.getExperience(1);
-        assertThat(actual).isNull();
+    @WithAnonymousUser
+    @Sql(scripts = "/sql/Experience/1.sql")
+    @Sql(scripts = "/sql/Experience/2.sql")
+    void getExperience_as_anon() throws Exception {
+        var actual1 = service.getExperience(1);
+        assertThat(actual1).isNull();
 
         var actual2 = service.getExperience(2);
-        var exp2 = objectMapper.readValue(new ClassPathResource("/json/experience/2.json").getInputStream(), Experience.class);
-        assertThat(actual2).usingRecursiveComparison().isEqualTo(exp2);
+        var expected2 = objectMapper.readValue(new ClassPathResource("/json/Experience/2.json").getInputStream(), Experience.class);
+        assertThat(actual2).usingRecursiveComparison().isEqualTo(expected2);
 
         var actual3 = service.getExperience(3);
         assertThat(actual3).isNull();
