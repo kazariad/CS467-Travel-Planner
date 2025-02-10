6fb20069c4bbe9a45bc6a31906387d5b8e54ebea
David K <122331465+kazariad@users.noreply.github.com>
2025-02-10 12:37:36 -0800

    Add mockable Clock & spring-security-test lib

---
+++ src/main/java/edu/oregonstate/cs467/travelplanner/config/AppConfig.java
@@ -0,0 +1,14 @@
+package edu.oregonstate.cs467.travelplanner.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+}
--- src/test/java/edu/oregonstate/cs467/travelplanner/AbstractBaseTest.java
+++ src/test/java/edu/oregonstate/cs467/travelplanner/AbstractBaseTest.java
@@ -3,9 +3,15 @@ package edu.oregonstate.cs467.travelplanner;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockReset;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+
+import java.time.Clock;
 
 @SpringBootTest
 public abstract class AbstractBaseTest {
     @Autowired
     protected ObjectMapper objectMapper;
+    @MockitoSpyBean(reset = MockReset.AFTER)
+    protected Clock clock;
 }
