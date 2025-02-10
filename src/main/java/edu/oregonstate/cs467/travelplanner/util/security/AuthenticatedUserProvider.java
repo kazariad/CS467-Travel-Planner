0e98ac490e49f2fc98fd53523e4a3f5d04dae70e
David K <122331465+kazariad@users.noreply.github.com>
2025-02-10 11:18:34 -0800

    Add AuthenticatedUserProvider

---
+++ src/main/java/edu/oregonstate/cs467/travelplanner/util/security/AuthenticatedUserProvider.java
@@ -0,0 +1,31 @@
+package edu.oregonstate.cs467.travelplanner.util.security;
+
+import edu.oregonstate.cs467.travelplanner.user.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class AuthenticatedUserProvider {
+    public User getUser() {
+        Authentication authentication = getAuthentication();
+        if (authentication == null) return null;
+        if (authentication.getPrincipal() instanceof User user) return user;
+        return null;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Authentication authentication = getAuthentication();
+        if (authentication == null) return null;
+        return authentication.getAuthorities();
+    }
+
+    Authentication getAuthentication() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        return securityContext == null ? null : securityContext.getAuthentication();
+    }
+}
