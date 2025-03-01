8539ccb705f2bd7d2a47bf05e9e5098b7c5c0173
David K <122331465+kazariad@users.noreply.github.com>
2025-02-28 16:28:15 -0800

    Add ExperienceSearch

---
+++ src/test/resources/sql/Experience/3.sql
@@ -0,0 +1,15 @@
+INSERT INTO `experience`
+VALUES (3,
+        'Ripple lantern horizon',
+        'echo breeze wander dusk velvet cobblestone ember',
+        '2025-01-01',
+        ST_PointFromText('POINT(34.04092768834788 -118.44286323459173)', 4326),
+        '2050 Sawtelle Blvd, Los Angeles, CA 90025',
+        NULL,
+        NULL,
+        0,
+        0,
+        1,
+        '2025-02-02 00:00:00.000000',
+        NULL,
+        NULL);
