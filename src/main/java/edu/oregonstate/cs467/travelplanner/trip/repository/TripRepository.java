3f9b4aa488633f05950d6face781f2834d20098e
theadinh59 <theadinh59@gmail.com>
2025-03-02 20:55:35 -0600

    repository for Trip

---
+++ src/main/java/edu/oregonstate/cs467/travelplanner/trip/repository/TripRepository.java
@@ -0,0 +1,12 @@
+package edu.oregonstate.cs467.travelplanner.trip.repository;
+
+import edu.oregonstate.cs467.travelplanner.trip.model.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TripRepository extends JpaRepository<Trip, Long> {
+    Optional<Trip> findByUserId(Long userId);
+}
