d40bbd36536a8a6999858a2cc88b21b9c0525b09
theadinh59 <theadinh59@gmail.com>
2025-03-03 16:24:21 -0600

    update repo's method for TripRepository

--- src/main/java/edu/oregonstate/cs467/travelplanner/trip/repository/TripRepository.java
+++ src/main/java/edu/oregonstate/cs467/travelplanner/trip/repository/TripRepository.java
@@ -5,10 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface TripRepository extends JpaRepository<Trip, Long> {
-    @Query("SELECT t FROM Trip t WHERE t.user.userId = :userId")
-    Optional<Trip> findByUserId(Long userId);
+    @Query("SELECT * FROM Trip t WHERE t.user.userId = :userId")
+    List<Trip> findAllByUserId(Long userId);
 }
