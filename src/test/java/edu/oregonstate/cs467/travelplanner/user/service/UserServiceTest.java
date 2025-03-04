e5d5c04eeaf149198e20de2bf5adc623e4588440
theadinh59 <theadinh59@gmail.com>
2025-03-03 18:26:37 -0600

    update testGetUserProfile() after making changes to Trip class

--- src/test/java/edu/oregonstate/cs467/travelplanner/user/service/UserServiceTest.java
+++ src/test/java/edu/oregonstate/cs467/travelplanner/user/service/UserServiceTest.java
@@ -222,7 +222,7 @@ public class UserServiceTest {
         trip.setTripTitle("California Beach Trip");
         trip.setStartDate(LocalDate.of(2024, 12, 15));
         trip.setEndDate(LocalDate.of(2024, 12, 20));
-        trip.setExperienceList(Set.of(experience1));
+        trip.setExperienceList(Set.of(experience1.getExperienceId()));
 
         // Mock the services to return the expected data
         when(experienceService.findByUserId(1L)).thenReturn(List.of(experience1));
