ee0417bb8cf08ebcae6203b97685dc035f56dd3b
theadinh59 <theadinh59@gmail.com>
2025-03-06 22:49:16 -0600

    Update query to exclude deleted trips and add pagination.

--- src/main/java/edu/oregonstate/cs467/travelplanner/trip/repository/TripRepository.java
+++ src/main/java/edu/oregonstate/cs467/travelplanner/trip/repository/TripRepository.java
@@ -14,6 +14,6 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
     @Query("SELECT t FROM Trip t WHERE t.user.userId = :userId")
     List<Trip> findAllByUserId(@Param("userId") Long userId);
 
-    @Query("SELECT t FROM Trip t WHERE t.user.userId = :userId ORDER BY t.createdAt DESC")
-    List<Trip> findTop10ByUserIdOrderByCreatedDateDesc(Long userId);
+    @Query("SELECT t FROM Trip t WHERE t.user.userId = :userId AND t.deletedAt IS NULL ORDER BY t.createdAt DESC")
+    List<Trip> findTop10ByUserIdOrderByCreatedDateDesc(Long userId, Pageable pageable);
 }
