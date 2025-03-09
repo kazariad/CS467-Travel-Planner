77023ff3a37c29973462533279adbeeafab0931c
records-t <148019253+records-t@users.noreply.github.com>
2025-03-09 03:32:12 -0700

    Add home page

--- src/main/java/edu/oregonstate/cs467/travelplanner/web/controller/MainController.java
+++ src/main/java/edu/oregonstate/cs467/travelplanner/web/controller/MainController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
-
 }
